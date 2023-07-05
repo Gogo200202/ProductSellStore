3be49be09c8dae520d2d8808ffedf5eff4a7f06e
Gogo200202 <gosheto420@gmail.com>
2023-07-05 18:42:22 +0300

    make viwe of of all item and detal look better

---
+++ ProductSellStore/Controllers/Orders.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductSellStore.Controllers
+{
+    public class Orders : Controller
+    {
+        public IActionResult AddOrder()
+        {
+            Console.WriteLine("asdasdas");
+            return RedirectToAction("All","Products");
+        }
+    }
+}
